240c12c0b7c21c988f9a348a85530aaa842ba674
Antoine <achatain@users.noreply.github.com>
2021-03-12 02:08:05 +0000

    Reinstate Gradle wrapper

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ popd
 echo '\n====\nBuilding the application...'
 docker build -t bonjoursoftware/mycollections:local .
 CONTAINER_ID=$(docker create bonjoursoftware/mycollections:local)
-docker cp $CONTAINER_ID:mycollections.jar ./mycollections.jar
+docker cp $CONTAINER_ID:mycollections.jar ./mycollections-1.2.5-all.jar
 docker container rm $CONTAINER_ID
