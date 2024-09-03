e783169f49be3abb3cefed2e7756f8d3eb961a32
Antoine <achatain@users.noreply.github.com>
2024-09-03 17:50:29 +0100

    Dependencies maintenance

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-echo '\n====\nBuilding the user interface...'
+echo 'Building the user interface...'
 pushd ui || exit
 ./build.sh
 popd || exit
 
-echo '\n====\nBuilding the application...'
-docker build -t bonjoursoftware/mycollections:local .
-CONTAINER_ID=$(docker create bonjoursoftware/mycollections:local)
-docker cp "$CONTAINER_ID":mycollections.jar ./mycollections-1.5.0-all.jar
+echo 'Building the application...'
+docker build --no-cache -t bonjoursoftware/mycollections:local .
+CONTAINER_ID=$(docker create docker.io/bonjoursoftware/mycollections:local)
+docker cp "$CONTAINER_ID":mycollections.jar ./mycollections-1.5.1-all.jar
 docker container rm "$CONTAINER_ID"
