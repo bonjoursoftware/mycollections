45deaf67e8c60f2a6b18e69cadfbfc7ca361db50
Antoine <achatain@users.noreply.github.com>
2025-07-01 19:57:46 +0100

    Replace Sendgrid notification service with Gmail notification service

--- src/main/groovy/com/bonjoursoftware/mycollections/BeanFactory.groovy
+++ src/main/groovy/com/bonjoursoftware/mycollections/BeanFactory.groovy
@@ -23,7 +23,7 @@
  */
 package com.bonjoursoftware.mycollections
 
-import com.bonjoursoftware.mycollections.notification.SendgridEmailNotificationService
+import com.bonjoursoftware.mycollections.notification.GmailNotificationService
 import com.bonjoursoftware.mycollections.notification.NotificationService
 import com.mongodb.ConnectionString
 import com.mongodb.MongoClientSettings
@@ -61,6 +61,6 @@ class BeanFactory {
             @Property(name = 'notification.target') String target
     ) {
         if (type != 'email') throw new RuntimeException("Notification type [$type] is not supported")
-        new SendgridEmailNotificationService(apiKey: apiKey, source: source, target: target)
+        new GmailNotificationService(apiKey: apiKey, source: source, target: target)
     }
 }
